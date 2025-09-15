3401656e4df9434f7f930481fe84bcd291058f84
zaikaman <91252891+zaikaman@users.noreply.github.com>
2025-09-15 12:09:19 +0700

    Fix build error 3

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { cn } from "@/lib/utils";
 
 interface LabelProps {
   children: React.ReactNode;
   htmlFor: string;
+  className?: string;
 }
 
-export const Label = ({ children, htmlFor }: LabelProps) => {
+export const Label = ({ children, htmlFor, className }: LabelProps) => {
   return (
-    <label htmlFor={htmlFor} className="text-sm font-medium">
+    <label htmlFor={htmlFor} className={cn("text-sm font-medium", className)}>
       {children}
     </label>
   );
-}; 
+};
